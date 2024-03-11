b99cf9d22977e63f0d079a6f1b4d19fdcb4c6038
f0rbit <dev@forbit.dev>
2024-03-11 20:07:48 +1030

    update diff script
    uses absolute paths now

--- bun-javascript/diff.sh
+++ bun-javascript/diff.sh
@@ -1,3 +1,3 @@
-bun src/parser.js --dir "../resources/codebase" --config "../config.json" > output-base.json
-bun src/parser.js --dir "../resources/codebase-changed" --config "../config.json" > output-new.json
+bun src/parser.js --dir "/home/tom/dev/github/todo-tracker/resources/codebase" --config "/home/tom/dev/github/todo-tracker/resources/config.json" > output-base.json
+bun src/parser.js --dir "/home/tom/dev/github/todo-tracker/resources/codebase-changed" --config "/home/tom/dev/github/todo-tracker/resources/config.json" > output-new.json
 bun src/diff.js
