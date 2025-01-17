42a82f70c091f1b8e47f6a5103ab011716e8fda1
f0rbit <dev@forbit.dev>
2025-01-17 21:51:35 +1030

    fix: use relative paths

--- diff.sh
+++ diff.sh
@@ -1,3 +1,3 @@
-bun src/parser.ts --dir "../resources/codebase" --config "../resources/config.json" > output-base.json
-bun src/parser.ts --dir "../resources/codebase-changed" --config "../resources/config.json" > output-new.json
+bun src/parser.ts --dir "./resources/codebase" --config "./resources/config.json" > output-base.json
+bun src/parser.ts --dir "./resources/codebase-changed" --config "./resources/config.json" > output-new.json
 bun src/diff.ts
